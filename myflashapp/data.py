fbe42ab9a8c82acd391960fb756d94649cde5fd0
Sher <sophat.chhay@gmail.com>
2020-07-08 17:50:18 +0700

    add new init python with flask framework

---
+++ myflashapp/data.py
@@ -0,0 +1,26 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title':'Article One',
+            'body':'hello ===================',
+            'author':'Sophat Chhay',
+            'create_date':'02/07/2020'
+        },
+        {
+            'id': 2,
+            'title':'Article Two',
+            'body':'hello ===================',
+            'author':'Cheng Sea',
+            'create_date':'02/07/2020'
+        },
+        {
+            'id': 3,
+            'title':'Article Three',
+            'body':'hello ===================',
+            'author':'Meng Heang',
+            'create_date':'02/07/2020'
+        }
+    ]
+
+    return articles
